d55973170b348e095c4305b8d33a811920555ffb
Ashwin-Dev-P <ashwindev1462001@gmail.com>
2023-05-01 23:59:02 +0530

    Movies UI change and js

---
+++ wwwroot/js/movies/MovieList.js
@@ -0,0 +1,13 @@
+﻿// To toggle show more or less of description of the movies
+function expandDescription(anchorElement, id) {
+    document.getElementById(id).classList.toggle('expand');
+
+    var innerText = anchorElement.innerHTML;
+
+    if (innerText == 'Show more...') {
+        anchorElement.innerHTML = "Show less";
+    } else {
+        anchorElement.innerHTML = "Show more...";
+    }
+
+}
